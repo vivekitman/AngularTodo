f9a98a11c70295c71f49fed1fd54603832dcaeba
Vivek Gupta <64057657+vivekitman@users.noreply.github.com>
2026-04-20 21:47:09 +0530

    Add files via upload

---
+++ card.ts
@@ -0,0 +1,14 @@
+import { Component ,input} from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  imports: [],
+  templateUrl: './card.html',
+  styleUrl: './card.css',
+})
+export class Card {
+
+  Title = input<string>();
+  Discription = input<string>();
+
+}
